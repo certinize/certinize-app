528eb9804646e214c58ab7313d7841a46e53316a
Hju Kneyck Flores <hjucode@gmail.com>
2022-09-25 13:18:49 +0800

    Integ. side nav component

--- src/pages/Transfer/Success.js
+++ src/pages/Transfer/Success.js
@@ -1,31 +1,29 @@
-import React, {  } from "react";
+import SideNavBar from "../../components/SideNavBar/SideNavBar";
 import "./Success.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+
 const SuccessPage = () => {
-    
-	return (
-        <div>
-            <div className="title">
-                <p>Distribute Certificate </p>
-            </div>
-            <hr/>
-            <body>
-                <div className="success-box">
-                    <div className="content">
-                        <img className="logo" src="./img/certinize-logo.png"/>
-                        <img className="check" src="./img/check.png"/>  
-                    </div>
-                </div>
-                <div className="success-msg">
-                    <p className="msg">Certificate sent Successfully</p>
-                    <button className="return-btn" type="submit">Return Home</button>
-                </div>
-                
-                
-            </body>
+  return (
+    <div className="page-container">
+      <SideNavBar />
+      <div className="title">
+        <div className="success-box">
+          <div className="content">
+            <img className="logo" src="./img/certinize-logo.png" />
+            <img className="check" src="./img/check.png" />
+          </div>
         </div>
-        
-	);
+        <div className="success-msg">
+          <p className="msg">Certificate sent Successfully</p>
+          <button className="return-btn" type="submit">
+            Return Home
+          </button>
+        </div>
+      </div>
+
+      <hr />
+    </div>
+  );
 };
 
 export default SuccessPage;
