9e78c5c636aceaa98cbf8d0ed5667d339e86906f
Hju Kneyck Flores <hjucode@gmail.com>
2022-09-25 15:48:07 +0800

    Add header component

--- src/pages/Transfer/Success.js
+++ src/pages/Transfer/Success.js
@@ -1,27 +1,33 @@
+import Header from "../../components/Header/Header";
 import SideNavBar from "../../components/SideNavBar/SideNavBar";
 import "./Success.css";
-import React from "react";
+import React, { useState } from "react";
 
 const SuccessPage = () => {
+  const [isExpanded, setExpendState] = useState(false);
+
   return (
     <div className="page-container">
-      <SideNavBar />
-      <div className="title">
-        <div className="success-box">
-          <div className="content">
-            <img className="logo" src="./img/certinize-logo.png" />
-            <img className="check" src="./img/check.png" />
+      <SideNavBar setExpanded={setExpendState} />
+      <div className="content">
+        <Header title="DISTRIBUTE CERTIFICATE" />
+        <div className={isExpanded ? "main-content" : "main-content-NX"}>
+          <div>
+            <div className="success-box">
+              <div className="logo-container">
+                <img className="logo" src="./img/certinize-logo.png" />
+                <img className="check" src="./img/check.png" />
+              </div>
+            </div>
+            <div className="success-msg">
+              <p className="msg">Certificate sent Successfully</p>
+              <button className="return-btn" type="submit">
+                Return Home
+              </button>
+            </div>
           </div>
         </div>
-        <div className="success-msg">
-          <p className="msg">Certificate sent Successfully</p>
-          <button className="return-btn" type="submit">
-            Return Home
-          </button>
-        </div>
       </div>
-
-      <hr />
     </div>
   );
 };
