3f7cc1cc868d5a1b4d37c6e05393e28a1920d961
Hju Kneyck Flores <hjucode@gmail.com>
2022-10-11 16:12:41 +0800

    Impl. preview page (closes #15)

--- src/pages/Editor/Editor.js
+++ src/pages/Editor/Editor.js
@@ -1,16 +1,18 @@
 import Button from "../../components/Button/Button";
 import ToolCategory from "../../components/ToolCategory/ToolCategory";
 import "./Editor.css";
+import { toPng } from "html-to-image";
 import React from "react";
+import { createRoot } from "react-dom/client";
 import Draggable from "react-draggable";
 
-const fontStyles = [
+const FONT_STYLES = [
   { value: "arial", label: "Arial" },
   { value: "times", label: "Times New Roman" },
   { value: "courier", label: "Courier New" },
 ];
 
-const fontSizes = [
+const FONT_SIZES = [
   { value: "12", label: "12" },
   { value: "14", label: "14" },
   { value: "16", label: "16" },
@@ -30,6 +32,8 @@ const fontSizes = [
   { value: "70", label: "70" },
 ];
 
+const BUTTON_STYLE = { width: "10%", height: "3em", fontSize: "1.5em" };
+
 export default class Editor extends React.Component {
   constructor(props) {
     super(props);
@@ -37,18 +41,40 @@ export default class Editor extends React.Component {
     this.date = React.createRef();
   }
 
-  eventLogger(e, data) {
-    console.log("Event: ", e);
-    console.log("Data: ", data);
-  }
-
   updateFontStyle(ref, fontStyle = "arial") {
     ref.current.style.fontFamily = fontStyle;
   }
 
   updateFontSize(ref, fontSize = 12) {
-    console.log(fontSize);
     ref.current.style.fontSize = `${fontSize}px`;
+  }
+
+  generateImage() {
+    toPng(document.getElementById("certificateBox"), {
+      quality: 1,
+    }).then(function (dataUrl) {
+      let image = new Image();
+      image.src = dataUrl;
+      image.alt = "Certificate Preview";
+
+      document.getElementsByClassName("tool-menu")[0].remove();
+
+      let btnContainer = createRoot(
+        document.getElementsByClassName("btn-container")[0]
+      );
+
+      btnContainer.render(
+        <div className="btn-container">
+          <Button
+            text="Transfer"
+            style={{ marginLeft: "1em", marginRight: "1em", ...BUTTON_STYLE }}
+          />
+          <Button text="Cancel" style={BUTTON_STYLE} styleType="danger" />
+        </div>
+      );
+
+      document.getElementById("certificateBox").replaceWith(image);
+    });
   }
 
   render() {
@@ -56,7 +82,7 @@ export default class Editor extends React.Component {
       <div className="primary-container">
         <div className="editor-container">
           <div className="ecert-container">
-            <div className="box">
+            <div className="box" id="certificateBox">
               <Draggable bounds="parent" defaultPosition={{ x: 0, y: 0 }}>
                 <div className="handle" ref={this.recipientName}>
                   <div className="box__title">
@@ -76,15 +102,16 @@ export default class Editor extends React.Component {
           <div className="btn-container">
             <Button
               text="Next"
-              style={{ width: "10%", height: "3em", fontSize: "1.5em" }}
+              style={BUTTON_STYLE}
+              onClick={this.generateImage}
             />
           </div>
         </div>
         <div className="tool-menu">
           <ToolCategory
             label={"Recipient Name"}
-            first={fontStyles}
-            second={fontSizes}
+            first={FONT_STYLES}
+            second={FONT_SIZES}
             firstCallback={(option) => {
               this.updateFontStyle(this.recipientName, option.value);
             }}
@@ -94,8 +121,8 @@ export default class Editor extends React.Component {
           />
           <ToolCategory
             label={"Date"}
-            first={fontStyles}
-            second={fontSizes}
+            first={FONT_STYLES}
+            second={FONT_SIZES}
             firstCallback={(option) => {
               this.updateFontStyle(this.date, option.value);
             }}
