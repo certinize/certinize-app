949a39351e57c5b949928b7a7aa674908119f877
Ars3nicc <franzmanrique21@gmail.com>
2022-10-15 14:25:56 +0800

    navbar modified

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,72 +1,35 @@
+import React, { } from "react";
 import "./index.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BiTransfer } from "react-icons/bi";
-import { FaCertificate, FaUserCircle } from "react-icons/fa";
+import { FaCertificate } from "react-icons/fa";
 import { RiTeamFill } from "react-icons/ri";
 
 const NavBar = () => {
-  return (
-    <div>
-      <nav className="navbar">
-        <div className="nav-header">
-          <img className="logo" src="./img/certinize-logo.png" alt="" />
-          <span className="certinize">Certinize</span>
-        </div>
-        <div className="nav-link">
-          <ul>
-            <li>
-              <a href="">
-                <RiTeamFill className="icon" />
-                About Us
-              </a>
-            </li>
-            <li>
-              <a href="">
-                <FaUserCircle className="icon" />
-                Profile
-              </a>
-            </li>
-            <li>
-              <a href="">
-                <FaCertificate className="icon" />
-                Certificates
-              </a>
-            </li>
-            <li>
-              <a href="">
-                <BiTransfer className="icon" />
-                Transfer
-              </a>
-            </li>
-          </ul>
-        </div>
-
-        <div className="user-profile">
-          <img className="user-icon" src="./img/default_user.png" alt="" />
-          <div className="profile-display">
-            <ul>
-              <li>
-                <div className="flex-icon">
-                  <img
-                    className="profile-icon"
-                    src="./img/default_user.png"
-                    alt=""
-                  />
+	return (
+        <div>
+            <nav>
+                <input id="nav-toggle" type="checkbox"/>
+                <div className="logo">
+                    <img src="./img/certinize-logo.png" alt="" /> 
+                    Certinize
                 </div>
+                <ul className="links">
+                    <li><a href="#home"><BiTransfer className="icon"/> Transfer</a></li>
+                    <li><a href="#about"><FaCertificate className="icon"/>Certificates</a></li>
+                    <li><a href="#work"><RiTeamFill className="icon"/>About Us</a></li>
+                    <img className="user-icon" src="./img/default_user.png" alt=""/>
+                </ul>
 
-                <div className="profile-name">
-                  <p>Unnamed User</p>
-                  <p>#12345</p>
-                </div>
-                <button type="button">View Profile</button>
-              </li>
-            </ul>
-          </div>
+                <label htmlFor="nav-toggle" className="icon-burger">
+                    <div className="line"></div>
+                    <div className="line"></div>
+                    <div className="line"></div>
+                </label>
+            </nav>
         </div>
-      </nav>
-    </div>
-  );
+	);
 };
 
 export default NavBar;
+
