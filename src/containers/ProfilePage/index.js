15bc130ebd2341897db247c83ca796aea5ab74c7
Hju Kneyck Flores <hjucode@gmail.com>
2022-10-22 01:17:08 +0800

    Use css classes instead of html tag overrides

--- src/containers/ProfilePage/index.js
+++ src/containers/ProfilePage/index.js
@@ -1,47 +1,46 @@
-import React, { } from "react";
+import Button from "../../components/Button";
+import Header from "../../components/Header/Header";
 import "./index.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
 
 const ProfilePage = () => {
-	return (
-        <>
-        <header>
-            <p>My Profile</p>
-        </header>
-        <div className="main">
-            <div className="user">
-                <div className="profile">
-                    <img src="./img/default_user2.png" alt="" />   
-                </div>
-                <div className="profile-text">
-                    <p>Unnamed User</p>
-                    <p>Joined since 2022</p>
-                </div>
-                <div className="text-center">
-                    <input type="submit" name="orgID" value="Return Home"/>
-                </div>
-                        
-            </div>
-            <div className="details">
-                <p className="text">My verification links...</p>
-                <form>
-                    <label>Wallet Address</label>
-                    <p>B7KNk9UWUGig89N-muCvuzNcboNmOYCOto@B52638HU</p>
-                    <label>Email Address</label>
-                    <p>user@gmail.com</p>
-                    <label>Official Website</label>
-                    <p>samplelink.com</p>
-                    <label>Google Trend Link</label>
-                    <p>trend link</p>
-                </form>
-            </div>
+  return (
+    <>
+      <Header title="My Profile" />
+      <div className="main">
+        <div className="user">
+          <div className="profile-page-avatar-container">
+            <img
+              className="profile-page-avatar"
+              src="./img/default_user2.png"
+              alt=""
+            />
+          </div>
+          <div className="profile-text">
+            <p>Unnamed User</p>
+            <p>Joined since 2022</p>
+          </div>
+          <div className="profile-page-return-btn">
+            <Button>Return Home</Button>
+          </div>
         </div>
-
-
-
-        </>
-	);
+        <div className="details">
+          <p className="text">My verification links...</p>
+          <form className="profile-page-form">
+            <label>Wallet Address</label>
+            <p>B7KNk9UWUGig89N-muCvuzNcboNmOYCOto@B52638HU</p>
+            <label>Email Address</label>
+            <p>user@gmail.com</p>
+            <label>Official Website</label>
+            <p>samplelink.com</p>
+            <label>Google Trend Link</label>
+            <p>trend link</p>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ProfilePage;
-
