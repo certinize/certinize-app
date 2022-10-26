60da64346be58319f49f9fdaa58812f594a4011a
Ars3nicc <franzmanrique21@gmail.com>
2022-10-26 21:14:34 +0800

    navbar css

---
+++ src/components/Options/index.js
@@ -0,0 +1,24 @@
+import styles from "./index.css";
+import PropTypes from "prop-types";
+import React from "react";
+
+const OptionMenu = ({ options }) => {
+  const createListItems = () => {
+    return options.map((option) => (
+      <li className={styles.option} key={option.id}>
+        <a href={option.link}>
+          {option.icon}
+          {option.name}
+        </a>
+      </li>
+    ));
+  };
+
+  return <ul className={styles.optionMenu}>{createListItems()}</ul>;
+};
+
+OptionMenu.propTypes = {
+  options: PropTypes.array.isRequired,
+};
+
+export default OptionMenu;
